17b47d11be07d304594e2682a2f845e405072b26
Burhanuddin Rashid <burhanrashid5253@gmail.com>
2020-11-24 07:59:25 +0530

    Adding dependencies

---
+++ app/src/main/java/ja/burhanrashid52/nasa/apod/ApodApp.kt
@@ -0,0 +1,10 @@
+package ja.burhanrashid52.nasa.apod
+
+import android.app.Application
+import org.koin.core.context.startKoin
+
+class ApodApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
