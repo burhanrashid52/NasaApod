52215211706f74eddb2ee857b6a8e7f32efe2d72
Burhanuddin Rashid <burhanrashid5253@gmail.com>
2020-11-24 07:44:57 +0530

    Init project

---
+++ app/src/main/java/ja/burhanrashid52/nasa/apod/MainActivity.kt
@@ -0,0 +1,11 @@
+package ja.burhanrashid52.nasa.apod
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
